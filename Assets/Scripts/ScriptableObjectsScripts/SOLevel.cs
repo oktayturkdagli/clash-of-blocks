4ec30d7131747e189a88f9e01b4630da7148340a
Oktay Türkdağlı <oktayturkdagli@gmail.com>
2022-07-06 16:44:36 +0300

    fix(levels): Levels are more stable.

--- Assets/Scripts/ScriptableObjectsScripts/SOLevel.cs
+++ Assets/Scripts/ScriptableObjectsScripts/SOLevel.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,8 @@ public class SOLevel : ScriptableObject
 {
     public List<LevelItem> levelGrid = new List<LevelItem>();
 
-    private int width = 5;
-    private int height = 5;
+    [SerializeField] private int width = 5;
+    [SerializeField] private int height = 5;
     public int Width { get => width; set => width = value; }
     public int Height { get => height; set => height = value; }
     
